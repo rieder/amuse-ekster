8a50e065fd3daf033c9849f45e94107d4ef1e0dc
Steven Rieder <steven@rieder.nl>
2019-02-20 13:58:27 +0000

    Stellar evolution code wrapper

--- stellar_evolution_class.py
+++ stellar_evolution_class.py
@@ -2,8 +2,58 @@
 from __future__ import print_function, division
 import logging
 from amuse.datamodel import Particles
+from amuse.community.sse.interface import SSE
+from amuse.community.seba.interface import SeBa
 
-logger = logging.getLogger(__name__)
+
+class StellarEvolutionCode(
+        object,
+):
+    "Stellar evolution object"
+
+    def __init__(
+            self,
+            evo_code=SSE,
+            logger=None,
+            **keyword_arguments
+    ):
+        self.typestr = "Evolution"
+        self.namestr = evo_code.__name__
+        self.logger = logger or logging.getLogger(__name__)
+
+        if evo_code is SSE:
+            self.code = SSE(
+                # channel_type="sockets",
+                **keyword_arguments
+            )
+        elif evo_code is SeBa:
+            self.code = SeBa(
+                **keyword_arguments
+            )
+        else:
+            self.code = evo_code(
+                **keyword_arguments
+            )
+        self.parameters = self.code.parameters
+
+    @property
+    def particles(self):
+        "return particles in the evolution code"
+        return self.code.particles
+
+    @property
+    def model_time(self):
+        "return the time of the evolution code"
+        return self.code.model_time
+
+    def evolve_model(self, tend):
+        "Evolve stellar evolution to time and sync"
+        return self.code.evolve_model(tend)
+
+    @property
+    def stop(self):
+        "Stop stellar evolution code"
+        return self.code.stop()
 
 
 class StellarEvolution(
@@ -15,13 +65,15 @@ class StellarEvolution(
             self,
             stars=None,
             evo_code=None,
+            logger=None,
     ):
+        self.logger = logger or logging.getLogger(__name__)
+
         if stars is None:
             self.star_particles = Particles()
         else:
             self.star_particles = stars
         if evo_code is None:
-            from amuse.community.sse.interface import SSE
             self.evo_code = SSE(
                 # channel_type="sockets",
             )
@@ -61,6 +113,42 @@ class StellarEvolution(
 
 
 def main():
+    "Test class with an IMF"
+    import sys
+    import numpy
+    from amuse.units import units
+
+    numpy.random.seed(11)
+
+    try:
+        from amuse_masc import make_a_star_cluster
+        use_masc = True
+    except ImportError:
+        use_masc = False
+
+    if len(sys.argv) > 1:
+        from amuse.io import read_set_from_file
+        stars = read_set_from_file(sys.argv[1], "amuse")
+    elif use_masc:
+        stars = make_a_star_cluster.new_cluster(number_of_stars=10001)
+    else:
+        from amuse.ic.salpeter import new_salpeter_mass_distribution
+        stars = Particles(10000)
+        stars.mass = new_salpeter_mass_distribution(len(stars))
+    print("Number of stars: %i" % (len(stars)))
+
+    code = StellarEvolutionCode(evo_code=SeBa)
+    code.particles.add_particles(stars)
+    print(code.parameters)
+    timestep = 0.2 | units.Myr
+    for step in range(10):
+        time = step * timestep
+        code.evolve_model(time)
+        print("Evolved to %s" % code.model_time.in_(units.Myr))
+        print("Most massive star: %s" % code.particles.mass.max())
+
+
+def _main():
     "Test class with an IMF"
     import numpy
     from amuse.ic.salpeter import new_salpeter_mass_distribution

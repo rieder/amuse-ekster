11f6002cdde4b15bae7b33e33447c5da91410741
Steven Rieder <steven@rieder.nl>
2020-11-27 20:51:41 +0000

    forgot to add this

---
+++ available_codes.py
@@ -0,0 +1,28 @@
+try:
+    from amuse.community.fi.interface import Fi
+except ImportError:
+    Fi = None
+try:
+    from amuse.community.bhtree.interface import BHTree
+except ImportError:
+    BHTree = None
+try:
+    from amuse.community.fastkick.interface import FastKick
+except ImportError:
+    FastKick = None
+try:
+    from amuse.community.hermite.interface import Hermite
+except ImportError:
+    Hermite = None
+try:
+    from amuse.community.ph4.interface import ph4
+except ImportError:
+    ph4 = None
+try:
+    from amuse.community.petar.interface import Petar
+except ImportError:
+    Petar = None
+try:
+    from amuse.community.phantom.interface import Phantom
+except ImportError:
+    Phantom = None

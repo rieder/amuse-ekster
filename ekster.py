5fce4b236c5291f3ceb35d7f1678beafeafaf6d9
Kong You Liow <kl457@lstar2.astro.ex.ac.uk>
2021-09-27 11:23:12 +0100

    add basic stroemgren method

--- ekster.py
+++ ekster.py
@@ -27,7 +27,7 @@ from gas_class import GasCode
 from stellar_dynamics_class import StellarDynamicsCode
 from stellar_evolution_class import StellarEvolutionCode
 from sinks_class import should_a_sink_form  # , sfe_to_density
-from feedback_class import test_stellar_feedback
+from feedback_class import test_stellar_feedback, basic_stroemgren_volume_method
 from plotting_class import plot_hydro_and_stars  # , plot_stars
 from plotting_class import (
     u_to_temperature, temperature_to_u,
@@ -1058,11 +1058,17 @@ class ClusterInPotential(
 
     def stellar_feedback(self):
         # Deliberately not doing anything here yet
-        #self.gas_particles = test_stellar_feedback(
-        #    gas_=self.gas_particles,
-        #    stars=self.star_particles
-        #)
-        print('Stellar feedback implemented.')
+        # self.gas_particles = test_stellar_feedback(
+        #     gas_=self.gas_particles,
+        #     stars=self.star_particles
+        # )
+        # print('Stellar feedback implemented.')
+
+        basic_stroemgren_volume_method(
+            gas_=self.gas_particles,
+            stars=self.star_particles
+        )
+
 
     def evolve_model(self, end_time):
         "Evolve system to specified time"
